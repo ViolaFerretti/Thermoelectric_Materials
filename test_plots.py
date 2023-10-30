fd58f7b66c6a7d1d1cbf6e62178f030889a82b83
violaferr1 <118218965+violaferr1@users.noreply.github.com>
2023-10-30 11:53:08 +0100

    updated tests after having passed function to create matrix of values as parameter to plotting function

--- test_plots.py
+++ test_plots.py
@@ -39,11 +39,13 @@ def test_create_matrix_DBM_sigma_DBMD():
 
     """
     
-    Y = sigma_DBMD
+    # input values
+    Y = sigma_DBMD 
     sub = 1
     delta = np.arange(0, 4, 1)
     eta = np.arange(-3, 3, 1)
     rk = 0
+    # expected output matrix of values
     expected_output = [[1., 1., 1., 1., 1., 1.],
                       [0.7534905, 0.59041838, 0.42060373, 0.34556551, 0.42060373, 0.59041838],
                       [0.46019411, 0.27986782, 0.15329349, 0.11051448, 0.15329349, 0.27986782],
@@ -71,11 +73,13 @@ def test_create_matrix_DBM_ke_DBMP():
 
     """
     
+    # input values
     Y = ke_DBMP
     sub = 3
     delta = np.arange(0, 4, 1)
     eta = np.arange(-3, 3, 1)
     rk = 0
+    # expected output matrix of values
     expected_output = [[19.96804623, 18.05317714, 19.60657931, 21.63702426, 19.60657931, 18.05317714],
                        [12.63071674, 10.59188483, 12.418841, 15.1329718, 12.418841, 10.59188483],
                        [7.14569757, 5.56937804, 7.1356376, 9.41843319, 7.1356376,  5.56937804],
@@ -104,11 +108,13 @@ def test_create_matrix_DBM_ZT_DBMD():
 
     """
     
+    # input values
     Y = ZT_DBMP
     sub = 4
     delta = np.arange(0, 3, 0.5)
     eta = np.arange(-2.5, 2.5, 1)
     rk = 2.5
+    # expected output matrix of values
     expected_output = [[0.23614468, 0.16976463, 0.02741111, 0.02741111, 0.16976463],
                       [0.34341195, 0.26076388, 0.04247588, 0.04247588, 0.26076388],
                       [0.48581822, 0.37299289, 0.05903655, 0.05903655, 0.37299289],
@@ -138,10 +144,13 @@ def test_create_matrix_SBM_S():
     ----------
 
     """
+    
+    # input values
     Y = S_SBMP
     sub = 2
     eta = np.arange(-6, 6, 0.5)
     rk = 0
+    # expected output matrix of values
     expected_output = [8.00123818, 7.50204015, 7.00336018, 6.50553065, 6.00909327, 5.51492438,
                        5.02442482, 4.5397906,  4.0643589,  3.60295946, 3.16207733, 2.74949659,
                        2.37313822, 2.03925882, 1.75083951, 1.50709078, 1.30422239, 1.13681683,
@@ -168,10 +177,13 @@ def test_create_matrix_SBM_ZT():
     ----------
 
     """
+    
+    # input values 
     Y = ZT_SBMP
     sub = 4
     eta = np.arange(-4.5, 5, 0.5)
     rk = 4
+    # expected output matrix of values
     expected_output = [0.05828404, 0.08155155, 0.112267,   0.15147145, 0.19926791, 0.25390594,
                        0.31078345, 0.36206354, 0.39802635, 0.41066906, 0.39782816, 0.36440503,
                        0.31943175, 0.27168627, 0.22707774, 0.18840296, 0.15629091, 0.13023928,
